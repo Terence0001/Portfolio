fdc746b3075e7cb65a6f8adaefc1bdb1bf9da9e5
Terence0001 <111972075+Terence0001@users.noreply.github.com>
2022-11-13 01:37:59 +0400

    Update
    MAJ du contact php (test)

--- forms/contact.php
+++ forms/contact.php
@@ -7,10 +7,10 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'terence.devequi19@protonmail.com';
+  $receiving_email_address = 'contact@example.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
+    include('../assets/vendor/php-email-form/php-email-form.php');
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
   }
@@ -24,13 +24,14 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-
+  /*
   $contact->smtp = array(
     'host' => 'example.com',
     'username' => 'example',
     'password' => 'pass',
     'port' => '587'
   );
+  */
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
